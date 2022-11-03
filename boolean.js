02f6c2715036045f9a45fa37179937560bba4d19
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-03 11:19:06 -0400

    Add if conditionals to boolean.js

--- boolean.js
+++ boolean.js
@@ -1,10 +1,16 @@
-let temp = 32;
+let temp = 89;
 let isFreezing = temp <= 32;
 
-// console.log(isFreezing);
+if (temp <= 32){
+    console.log("It's freezing outside");
+}
+
+if (temp >= 110){
+    console.log("It is way too hot outside");
+}
 
 // create age set to your age
-let age = 65;
+let age = 5;
 // under 7 ischild discount
 let isChild = age <= 7;
 // over 65 issenior
@@ -16,3 +22,12 @@ console.log(isChild);
 console.log(isSenior);
 
 //7, 30, 65
+
+// if 7 or under print msg about child price
+if (age <= 7){
+    console.log("Children enter free!");
+}
+// if 65 or older print senior discount
+if (age >= 65){
+    console.log("Seniors get 75% discount");
+}
