b40903ad3f14b5cadeeac4889533b518bcd55735
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-03 12:05:47 -0400

    Add scope-2.js with variable shadowing and global leaking

---
+++ scope-2.js
@@ -0,0 +1,16 @@
+// Global (name)
+    // Local (name)
+        // Local
+    // Local
+
+let name = "Andrew";
+if (true){
+    let name = "Mike";
+    if (true){
+        console.log(name);
+    }
+}
+
+if (true){
+
+}
