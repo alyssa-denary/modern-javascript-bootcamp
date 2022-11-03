2226a4cc8bcfb4b55a1f36b3ec96b30ffc60e8ba
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-03 11:29:11 -0400

    Code boolean advanced

---
+++ boolean-advance.js
@@ -0,0 +1,25 @@
+let isAccountLocked = false;
+let userRole = "admin";
+
+if (isAccountLocked){
+    console.log("Account is locked")
+} else if (userRole === "admin"){
+    console.log("Welcome Admin!");
+} else {
+    console.log("Welcome!");
+}
+
+
+// temp 45
+let temp = 115;
+// freezing outside
+// hot outide
+// go for it, pretty nice
+
+if (temp <= 32){
+    console.log("It's freezing outside!");
+} else if (temp >= 110){
+    console.log("It's way too hot");
+} else {
+    console.log("Go for it, it's pretty nice.");
+}
