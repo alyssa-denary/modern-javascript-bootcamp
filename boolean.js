659840ce09fb55e762771fe65709fb90a2ee2524
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-03 11:11:55 -0400

    Code Intro to Booleans

---
+++ boolean.js
@@ -0,0 +1,18 @@
+let temp = 32;
+let isFreezing = temp <= 32;
+
+// console.log(isFreezing);
+
+// create age set to your age
+let age = 65;
+// under 7 ischild discount
+let isChild = age <= 7;
+// over 65 issenior
+let isSenior = age >= 65;
+
+// print ischild
+console.log(isChild);
+// print isSenior 
+console.log(isSenior);
+
+//7, 30, 65
