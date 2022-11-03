736a02fafcf11822615d18e3dfdad9e9380ccefb
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-03 11:01:52 -0400

    Code Temp converter

---
+++ temp-conversion.js
@@ -0,0 +1,12 @@
+let fahrenheit = 50;  // 0 celsius, 273.15 kelvin
+
+// calculate celsius and store in celsius variable
+let celsius = (fahrenheit - 32) * (5/9);
+// print the value
+console.log(celsius);
+
+// calculate the kelvin value and store in variable
+let kelvin = (fahrenheit - 32)/1.8 + 273.15
+// print that value
+console.log(kelvin);
+
