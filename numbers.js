fbdee3455eefc96063eb705b95e03d4ba8f66d35
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-03 10:47:33 -0400

    Code Intro to Numbers examples

---
+++ numbers.js
@@ -0,0 +1,20 @@
+let num = 5.5 * 2;
+console.log(num);
+
+let x = 11;
+let num1 = x + 1 * 2;
+console.log(num1);
+
+let age = 31;
+let dogYears = (age + 1) / 7;
+console.log(dogYears);
+
+// studentScore 18
+// maxScore 20
+// percent 90
+
+let studentScore = 20;
+let maxScore = 100;
+let percent = studentScore / maxScore * 100;
+
+console.log(percent);
