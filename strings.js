edbac76c6d161962ce446385427139e0944f956e
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-03 10:38:32 -0400

    Code Exercises for Intro Strings Section

---
+++ strings.js
@@ -0,0 +1,17 @@
+let name = "Alyssa Denary";
+
+console.log(name);
+
+let firstName = "Alyssa";
+let lastName = "Denary";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+//city, country, location
+
+let city = "New Haven";
+let country = "United States";
+let location = `${city}, ${country}`;
+
+console.log(location); 
