aa7b2e586d520869a66478f8ec5cd17e14e0009b
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-05 13:09:17 -0400

    Build grade calculator

---
+++ functions/grade-calc.js
@@ -0,0 +1,43 @@
+// students score, total possible score
+// 15/20  --> You got a C (75%)
+
+// function percent
+const percentCalculator = function (num1, num2){
+    return Math.round((num1/num2) * 100);
+}
+
+const lettergrade = function (percent){
+    if (percent >= 90) {
+        return "A";
+    } else if (percent >= 80) {
+        return "B";
+    } else if (percent >= 70) {
+        return "C";
+    } else if (percent >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }
+}
+
+// function letter grade calculator
+const gradeCalc = function(score, total){
+    let percent = percentCalculator(score, total);
+    let grade = lettergrade(percent);
+    return `You got a ${grade} (${percent}%)`
+}
+
+
+let tommyGrade = gradeCalc(60, 100);
+let susieGrade = gradeCalc(5, 10);
+let albertGrade = gradeCalc(93, 95);
+let bobGrade = gradeCalc(35, 50);
+let nanGrade = gradeCalc(4, 5);
+
+console.log(tommyGrade);
+console.log(susieGrade);
+console.log(albertGrade);
+console.log(bobGrade);
+console.log(nanGrade);
+
+
