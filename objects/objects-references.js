fa004a63ea5000096c5625f52c3223593c126566
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-07 11:06:54 -0500

    Add object-references

---
+++ objects/objects-references.js
@@ -0,0 +1,15 @@
+let myAccount = {
+    name: 'Alyssa',
+    expenses: 0,
+    income: 0
+}
+
+let addExpense = function (account, amt) {
+    account = {};
+    //account.expenses += amt;
+    console.log(account);
+}
+
+addExpense(myAccount, 2.5);
+console.log(myAccount);
+
