2dbd07d39b602dc6bc88550a7040cfdc16c80999
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-08 18:49:01 -0500

    Update to refactored code

--- code-reviews/isSubsequence.js
+++ code-reviews/isSubsequence.js
@@ -20,7 +20,12 @@ Space Complexity - O(1)
 function isSubsequence(str1, str2) {
     let index1 = 0;
     let index2 = 0;
-    function getCharMatch(num1, num2) {
+    while (index2 < str2.length && index1 < str1.length){
+        getMatchAndIncrementIndices(index1, index2);
+    }
+    return index1 === str1.length;
+    
+    function getMatchAndIncrementIndices(num1, num2) {
         if (str2[index2] === str1[index1]){
             index1++;
             index2++;
@@ -28,13 +33,9 @@ function isSubsequence(str1, str2) {
             index2++;
         }
     }
-    while (index2 < str2.length && index1 < str1.length){
-        getCharMatch(index1, index2);
-    }
-    return index1 === str1.length;
   }
 
-  
+
 console.log(isSubsequence('hello', 'hello world')); // true
 console.log(isSubsequence('sing', 'sting')); // true
 console.log(isSubsequence('abc', 'abracadabra')); // true
