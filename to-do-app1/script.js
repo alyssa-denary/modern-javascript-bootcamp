381268427f68d7ae2e2fba56c321c782bb9c514f
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-15 12:28:26 -0500

    style: delete previously version of named files to-do-app1

--- to-do-app1/script.js
+++
@@ -1,7 +0,0 @@
-const allParagraphs = document.querySelectorAll("p");
-allParagraphs.forEach((p) => {
-  if (p.textContent.includes(" the ")) {
-    // Spaces around the to exclude words with 'the' embedded (ex: other)
-    p.remove();
-  }
-});
