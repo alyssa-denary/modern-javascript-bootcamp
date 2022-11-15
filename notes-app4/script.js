b5dd38238aff4a3af0dc8f32dfd0662ed1e4c596
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-15 14:29:07 -0500

    add: notes-app4 for section divider

---
+++ notes-app4/script.js
@@ -0,0 +1,72 @@
+const notes = [
+  {
+    title: "My next trip",
+    body: "I would like to go to spain",
+  },
+  {
+    title: "Habits to work on",
+    body: "I should exercise more, Eating a bit better",
+  },
+  {
+    title: "Office modification",
+    body: "Get a new seat",
+  },
+  {
+    title: "A test for sorting",
+    body: "test test",
+  },
+];
+
+// Create filter object to update whenever input value changes
+const filter = {
+  searchText: "",
+};
+
+// Render all notes to start
+renderNote(notes);
+
+// Each time input changes, remove all notes and just show new rendered items
+// Get current state of search input and update filter
+// Render just the notes that match that filter
+document.querySelector("#filter-notes").addEventListener("input", function (e) {
+  removeAllNotes(".note");
+  filter.searchText = e.target.value;
+  renderNote(filterNotes(notes, filter));
+});
+
+// Remove button removes all notes
+document.querySelector("#remove-button").addEventListener("click", (e) => {
+  removeAllNotes(".note");
+});
+
+//////    HELPER FUNCTIONS ///////
+
+// Create function to filter notes: render to-do items to document:
+// input: notes, filters, output: array of objects of filtered notes
+function filterNotes(arrOfObj, filterObj) {
+  return arrOfObj.filter((noteObj) => {
+    return (
+      noteObj.title
+        .toLowerCase()
+        .includes(filterObj.searchText.toLowerCase()) ||
+      noteObj.body.toLowerCase().includes(filterObj.searchText.toLowerCase())
+    );
+  });
+}
+
+// create function to render notes to DOM
+// input: array of objects, output: each objects title rendered to DOM as paragraph
+function renderNote(arrOfObj) {
+  for (const obj of arrOfObj) {
+    const newPar = document.createElement("p");
+    newPar.textContent = `${obj.title}: ${obj.body}`;
+    newPar.className = "note";
+    document.querySelector("body").appendChild(newPar);
+  }
+}
+
+function removeAllNotes(selector) {
+  document.querySelectorAll(selector).forEach((note) => {
+    note.remove();
+  });
+}
