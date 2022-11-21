2cfeceaa684ab5cd9790b15c200fa2a7dccc81a6
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-21 11:03:38 -0500

    refactor: create separate file to hold functions

---
+++ notes-app6/script.js
@@ -0,0 +1,30 @@
+const notes = getSavedNotes();
+
+// Render all notes to start
+renderNote(notes);
+
+// Search form listener
+document.querySelector("#search-form").addEventListener("input", function (e) {
+  clearSection("#all-notes");
+  renderNote(filterNotes(notes, e.target.value));
+});
+
+// Create new note submit listener
+document
+  .querySelector("#create-note-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    const newNote = {
+      title: e.target.elements.title.value,
+      body: e.target.elements.body.value,
+    };
+    notes.push(newNote);
+    localStorage.setItem("notes", JSON.stringify(notes));
+    clearSection("#all-notes");
+    renderNote(notes);
+    e.target.elements.title.value = "";
+    e.target.elements.body.value = "";
+  });
+
+// Dropdown listener
+document.querySelector("");
