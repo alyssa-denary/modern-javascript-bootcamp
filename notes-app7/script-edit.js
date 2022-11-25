74c4febb6d64ff6270a4710ded705801c085ffd6
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-25 11:05:51 -0500

    add: notes edit page saves in real time

--- notes-app7/script-edit.js
+++ notes-app7/script-edit.js
@@ -2,8 +2,19 @@ const noteID = location.hash.substring(1);
 const notes = getSavedNotes();
 const note = notes.find((el) => el.id === noteID);
 
-if ((note = undefined)) {
+if (note === undefined) {
   location.assign(index.html);
 }
 
+document.querySelector("#note-title").value = note.title;
+document.querySelector("#note-body").value = note.body;
 
+document.querySelector("#note-title").addEventListener("input", (e) => {
+  note.title = e.target.value;
+  saveNote(notes);
+});
+
+document.querySelector("#note-body").addEventListener("input", (e) => {
+  note.body = e.target.value;
+  saveNote(notes);
+});
