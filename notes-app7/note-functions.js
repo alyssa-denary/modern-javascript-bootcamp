dc75a6bd13eb3835fdeab52042b0e9c37e3bdd30
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-11-25 08:47:35 -0500

    add: set up for notes-app7

---
+++ notes-app7/note-functions.js
@@ -0,0 +1,72 @@
+// Read existing notes from localStorage
+function getSavedNotes() {
+  const notesJSON = localStorage.getItem("notes");
+  if (notesJSON !== null) {
+    return JSON.parse(notesJSON);
+  } else {
+    return [];
+  }
+}
+
+function saveNote(notes) {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+// Filter notes that include string in title or body of note
+function filterNotes(arrOfObj, string) {
+  return arrOfObj.filter((noteObj) => {
+    return (
+      noteObj.title.toLowerCase().includes(string.toLowerCase()) ||
+      noteObj.body.toLowerCase().includes(string.toLowerCase())
+    );
+  });
+}
+
+// Generate note on DOM
+function generateNoteDOM(obj) {
+  const newSection = document.createElement("section");
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "X";
+  newSection.appendChild(removeButton);
+
+  removeButton.addEventListener("click", () => {
+    removeNote(obj.id);
+    renderFilteredNotes();
+  });
+
+  const newArticle = document.createElement("article");
+  newArticle.textContent = `${obj.title}: ${obj.body}`;
+  newArticle.setAttribute("class", "note");
+  newSection.appendChild(newArticle);
+
+  document.querySelector("#all-notes").appendChild(newSection);
+}
+
+// Render note to DOM
+function renderNote(arrOfObj) {
+  for (const obj of arrOfObj) {
+    generateNoteDOM(obj);
+  }
+}
+
+function renderFilteredNotes() {
+  const filtered = filterNotes(notes, userInput.searchText);
+  clearSection("#all-notes");
+  renderNote(filtered);
+}
+
+// Remove all notes from an html section
+function clearSection(selector) {
+  document.querySelector(selector).innerHTML = "";
+}
+
+// Remove specific note by id
+function removeNote(identifier) {
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].id === identifier) {
+      notes.splice(i, 1);
+    }
+  }
+  saveNote(notes);
+}
---
+++ notes-app7/script.js
@@ -0,0 +1,34 @@
+const notes = getSavedNotes();
+
+const userInput = { searchText: "" };
+
+// Render all notes to start
+renderFilteredNotes();
+
+// Search form listener
+document.querySelector("#search-form").addEventListener("input", function (e) {
+  userInput.searchText = e.target.value;
+  renderFilteredNotes();
+});
+
+// Create new note submit listener
+document
+  .querySelector("#create-note-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    const newNote = {
+      id: uuidv4(),
+      title: e.target.elements.title.value,
+      body: e.target.elements.body.value,
+    };
+    notes.push(newNote);
+    saveNote(notes);
+    renderFilteredNotes();
+    e.target.elements.title.value = "";
+    e.target.elements.body.value = "";
+  });
+
+// Dropdown listener
+document.querySelector("#sort-dropdown").addEventListener("change", (e) => {
+  console.log(e.target.value);
+});
