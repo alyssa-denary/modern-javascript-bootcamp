4e27ff17480018cfef67f17a2e4e4c07dc4b9850
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-12-02 12:16:20 -0500

    style: update title and h1

--- notes-app9/moment.js
+++
@@ -1,8 +0,0 @@
-const now = moment();
-console.log(now.toString());
-now.add(1, "year").subtract(20, "days");
-console.log(now.format("MMMM Do, YYYY"));
-console.log(now.fromNow());
-
-const nowTimestamp = now.valueOf();
-console.log(moment(nowTimestamp).toString());
