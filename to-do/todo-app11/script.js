7ed8a8158302cd51ed8acb5bf4f1ea314a218a4e
Alyssa Dunlop <alyssa.denary@gmail.com>
2022-12-10 09:32:00 -0500

    add: todo version 2.0 revamp

---
+++ to-do/todo-app11/script.js
@@ -0,0 +1,45 @@
+'use strict'
+
+// Retrieve saved todos
+const todos = getSavedTodos();
+
+// Variable to hold user input
+const userInput = {
+  newTask: "",
+  searchText: "",
+  hideCompleted: getHideVal(),
+};
+
+// Render initial todos
+renderFilteredTodos();
+
+// Listener for search text changes:
+document.querySelector("#search-input").addEventListener("input", function (e) {
+  userInput.searchText = e.target.value;
+  renderFilteredTodos();
+});
+
+// Listener for changes to hiding completed checkbox:
+document
+  .querySelector("#hide-completed")
+  .addEventListener("change", function (e) {
+    userInput.hideCompleted = e.target.checked;
+    saveHideVal(userInput.hideCompleted);
+    renderFilteredTodos();
+  });
+
+// Listener for submit button to add a new to-do
+document
+  .querySelector("#new-todo-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    const newTodoObj = {
+      id: uuidv4(),
+      text: e.target.elements.newTodo.value,
+      completed: false,
+    };
+    todos.push(newTodoObj);
+    saveTodos(todos);
+    renderFilteredTodos();
+    e.target.elements.newTodo.value = "";
+  });
---
+++ to-do/todo-app11/todo-functions.js
@@ -0,0 +1,116 @@
+'use strict'
+
+// Get saved todos
+const getSavedTodos = function () {
+  const todosJSON = localStorage.getItem("todos");
+  if (todosJSON !== null) {
+    return JSON.parse(todosJSON);
+  } else {
+    return [];
+  }
+};
+
+// Save todos in local storage
+const saveTodos = function (todos) {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+// Update hideVal based on checked status
+const getHideVal = () => {
+  const hideValJSON = localStorage.getItem("hideVal");
+  if (hideValJSON !== null) {
+    return JSON.parse(hideValJSON);
+  } else {
+    return false;
+  }
+};
+
+// Save hideVal in localStorage
+const saveHideVal = (val) => {
+  localStorage.setItem("hideVal", JSON.stringify(val));
+};
+
+// Filter to-do items based on Text AND if hide completed is checked
+function filterTodo(arrOfObj, textFilter, isHidden) {
+  return arrOfObj.filter((obj) =>
+    isHidden
+      ? obj.text.toLowerCase().includes(textFilter.toLowerCase()) &&
+        obj.completed === false
+      : obj.text.toLowerCase().includes(textFilter.toLowerCase())
+  );
+}
+
+// Generate todo DOM
+const generateTodoDOM = function (obj) {
+  const newTask = document.createElement("li");
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.addEventListener("change", (e) => {
+    toggleCompleted(obj.id);
+    saveTodos(todos);
+    renderFilteredTodos(todos);
+  });
+  checkbox.checked = obj.completed;
+
+  const button = document.createElement("button");
+  button.textContent = "X";
+  button.addEventListener("click", () => {
+    removeTodo(obj.id);
+    renderFilteredTodos();
+  });
+
+  newTask.appendChild(checkbox);
+  newTask.append(obj.text);
+  newTask.appendChild(button);
+
+  document.querySelector("#todo-list").appendChild(newTask);
+};
+
+// Render content to DOM
+function renderTodoItems(arrOfObj) {
+  arrOfObj.forEach((obj) => {
+    generateTodoDOM(obj);
+  });
+}
+
+// Renders summary to DOM
+function renderSummary(array) {
+  let incomplete = array.filter((el) => !el.completed);
+  document.querySelector(
+    "#summary"
+  ).textContent = `You have ${incomplete.length} to-do's left`;
+}
+
+function renderFilteredTodos() {
+  let filtered = filterTodo(
+    todos,
+    userInput.searchText,
+    userInput.hideCompleted
+  );
+  clearSection("#todo-list");
+  renderSummary(filtered);
+  renderTodoItems(filtered);
+}
+
+// Removes all elements from a section
+function clearSection(selector) {
+  document.querySelector(selector).innerHTML = "";
+}
+
+// Remove specific todo by id
+function removeTodo(identifier) {
+  const ind = todos.findIndex((el) => el.id === identifier);
+  if (ind !== -1) {
+    todos.splice(ind, 1);
+  }
+  saveTodos(todos);
+}
+
+// Toggle completed status by id
+function toggleCompleted(identifier) {
+  const match = todos.find((el) => el.id === identifier);
+  if (match !== undefined) {
+    match.completed = !match.completed;
+  }
+}
